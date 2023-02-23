ddc2669602af667662c37b9e2268049b18442107
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-23 02:05:13 +0200

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, cur) => {
+    return sum + cur.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
