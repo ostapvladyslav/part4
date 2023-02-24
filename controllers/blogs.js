5d95bb3ac544932c8f946a67444718f5ae875c2a
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-24 02:16:31 +0200

    Exercise 4.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -20,4 +20,18 @@ blogsRouter.post('/', async (req, res) => {
   res.status(201).json(savedBlog);
 });
 
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id);
+  res.status(204).end();
+});
+
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,25 +8,22 @@ const Blog = require('../models/blog');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog);
-    await blogObject.save();
-  }
+  await Blog.insertMany(helper.initialBlogs);
 });
 
-describe('blogs', () => {
+describe('when there is initially some blogs saved', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/);
-  }, 100000);
+  });
 
   test('all blogs are returned', async () => {
-    const res = await api.get('/api/blogs');
+    // const res = await api.get('/api/blogs');
+    const blogsAtStart = await helper.blogsInDb();
 
-    expect(res.body).toHaveLength(helper.initialBlogs.length);
+    expect(blogsAtStart).toHaveLength(helper.initialBlogs.length);
   });
 
   test('a specific blog is within the returned blogs', async () => {
@@ -42,8 +39,37 @@ describe('blogs', () => {
     expect(res.body[0].id).toBeDefined();
     expect(res.body[0]._id).toBeUndefined();
   });
+});
 
-  test('a valid blog can be added', async () => {
+describe('viewing a specifig blog', () => {
+  test('succeeds with a valid id', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+
+    const blogToView = blogsAtStart[0];
+
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(resultBlog.body).toEqual(blogToView);
+  });
+
+  test('fails with 404 if blog does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
+
+    await api.get(`/api/blogs/${validNonexistingId}`).expect(404);
+  });
+
+  test('fails with 400 if id is invalid', async () => {
+    const invalidId = 1234;
+
+    await api.get(`/api/blogs/${invalidId}`).expect(400);
+  });
+});
+
+describe('addition of a blog', () => {
+  test('succeeds with valid data', async () => {
     const newBlog = {
       title: 'Vladyslav Completes Fullstackopen Course',
       author: 'V. Ostapchuk',
@@ -64,7 +90,19 @@ describe('blogs', () => {
     expect(titles).toContainEqual('Vladyslav Completes Fullstackopen Course');
   });
 
-  test('blog missing "likes" property have 0 likes', async () => {
+  test('fails with 400 if data is invalid', async () => {
+    const invalidBlog = {
+      author: 'V. Ostapchuk',
+      likes: 50,
+    };
+
+    await api.post('/api/blogs').send(invalidBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('succeeds when blog missing "likes" property with 0 likes', async () => {
     const newBlogWithoutLikes = {
       title: 'Vladyslav Completes Fullstackopen Course',
       author: 'V. Ostapchuk',
@@ -81,14 +119,35 @@ describe('blogs', () => {
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
     expect(blogsAtEnd[helper.initialBlogs.length].likes).toBe(0);
   });
+});
 
-  test('blog without title or url is not added', async () => {
-    const invalidBlog = {
-      author: 'V. Ostapchuk',
-      likes: 50,
-    };
+describe('deletion of blog', () => {
+  test('succeeds with 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-    await api.post('/api/blogs').send(invalidBlog).expect(400);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+
+  test('succeeds with 204 if blog does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId();
+
+    await api.delete(`/api/blogs/${validNonexistingId}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('fails with 400 if id is invalid', async () => {
+    const invalidId = 1234;
+
+    await api.delete(`/api/blogs/${invalidId}`).expect(400);
 
     const blogsAtEnd = await helper.blogsInDb();
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
